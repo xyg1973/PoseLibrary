eb29024e098675138a2e0c1e6cb74ed88c413325
xyg1973 <18813757685@163.com>
2023-04-27 23:39:06 +0800

    batch ui update

--- temp.py
+++ temp.py
@@ -1,14 +1,20 @@
-from pymxs import runtime as rt
-import pymxs
+# from pymxs import runtime as rt
+# import pymxs
+#
+#
+# def ls():
+#     allobj = []
+#     objs = rt.selection
+#     for obj in objs:
+#         allobj.append(obj)
+#     return allobj
+#
+#
+# b = ls()[0]
+# print(b)
+import os
+import sys
 
+if __name__ == "__main__":
 
-def ls():
-    allobj = []
-    objs = rt.selection
-    for obj in objs:
-        allobj.append(obj)
-    return allobj
-
-
-b = ls()[0]
-b.transform = rt.Matrix3(1)
+    print("hello")
